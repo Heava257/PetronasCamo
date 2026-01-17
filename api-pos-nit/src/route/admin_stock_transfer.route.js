962acf68ba932b1d82302bcd90edbfcfd8315508
keochamraeun54@gmail.com <keochamraeun54@gmail.com>
2026-01-17 15:59:20 +0700

    fff

--- api-pos-nit/src/route/admin_stock_transfer.route.js
+++ api-pos-nit/src/route/admin_stock_transfer.route.js
@@ -8,11 +8,10 @@ const {
   // productImage,
 } = require("../controller/admin_stock_transfer.contoller");
 const { uploadFile } = require("../util/helper");
-
 module.exports = (app) => {
-  app.post("/api/admin_stock_transfer",validate_token(),create);
+  app.post("/api/admin_stock_transfer", validate_token(), create);
   app.get("/api/admin_stock_transfer", validate_token(), getList);
-  app.put("/api/admin_stock_transfer",validate_token(), update);
+  app.put("/api/admin_stock_transfer", validate_token(), update);
   app.delete("/api/admin_stock_transfer/:id", validate_token(), remove);
   app.post("/api/new_barcode", validate_token(), newBarcode);
   // app.get("/api/product_image/:product_id", validate_token(), productImage);
